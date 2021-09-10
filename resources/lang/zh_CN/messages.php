b32996ccf2eca37907e910e2f83def2b2282d62d
Lim@MacBook <teckwei1993@gmail.com>
2021-09-11 01:40:17 +0800

    feat: add Chinese locale

---
+++ resources/lang/zh_CN/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'invalid' => ':attribute 不是一个有效的密码',
+	'expired' => ':attribute 已经逾期。',
+	'max_attempt' => ':attribute 达到了允许的最大尝试次数。',
+];
---
+++ resources/lang/zh_HK/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'invalid' => ':attribute 並非一個有效的密碼',
+	'expired' => ':attribute 已經認期。',
+	'max_attempt' => ':attribute 達到了允許的最大嘗試次數。',
+];
