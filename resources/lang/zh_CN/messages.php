6405d18eb06f39b520a65b21927bc9794c7baf81
Zak <hello@zako.dev>
2022-03-08 14:41:59 +0100

    Code reformat based on PHPStorm auto-formatter and best practices.
    - Add newlines at EOF
    - Remove unused imports where neccessary
    - Add return type in Facade

--- resources/lang/zh_CN/messages.php
+++ resources/lang/zh_CN/messages.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-	'invalid' => ':attribute 不是一个有效的密码',
-	'expired' => ':attribute 已经逾期。',
-	'max_attempt' => ':attribute 达到了允许的最大尝试次数。',
+    'invalid' => ':attribute 不是一个有效的密码',
+    'expired' => ':attribute 已经逾期。',
+    'max_attempt' => ':attribute 达到了允许的最大尝试次数。',
 ];
